d924603c4c5972a670c177dcd6b1b02b09454397
Nikita Trofimov <nikita_givchik@mail.ru>
2019-11-06 20:21:10 +0300

    Выполнительное ДОПОЛНИТЕЛЬНОЕ задание № 1 в УРОКЕ № 4

--- lesson_4/hard/js/main.js
+++ lesson_4/hard/js/main.js
@@ -1,57 +1,48 @@
 'use strict';
 
-// Task 1
-let str = 'урок-3-был слишком легким';
+// Многомерные массивы ( [ [], [], ..] ) иногда называют матрицами. Напишите функцию, которая будет спрашивать у пользователя сколько массивов включить во внутрь arr, заполняет их несколькими произвольными числами (не больше 5) и выводит сумму элементов этого массива.
 
-str = str[0].toUpperCase() + str.slice(1);
+let arr = [];
 
-console.log(str);
-// Самый правильный вариант, т.к может работать с отриц. значениями
-
-// str = str[0].toUpperCase() + str.substring(1);
-
-// str = str[0].toUpperCase() + str.substr(1);
-
-// Task 2
-str = str.replace(/-/g, " ");
-
-console.log(str);
-
-// Task 3
-str = str.split(' ');
-str = str[str.length - 1].replace('им', 'о');
-
-console.log(str);
-
-// Task 4
-let arr = [20, 33, 1, 'Человек', 2, 3],
-	countThis,
-	sumCubes = 0;
-
-countThis = () => {
-	for (let i = 0; i < arr.length; i++) {
-		if ((typeof (arr[i]) === 'number')) {
-			sumCubes += Math.pow(arr[i], 3);
-		} else {
-			continue;
-		}
+// Функция, где спрашиваем количество массивов, которые хотим добавить в массив
+function countArrs() {
+	let num;
+	while (isNaN(num) || num == '' || num == null || num <= 0) {
+		num = +prompt("Сколько массивов включить в наш массив?");
 	}
-	return sumCubes = Math.sqrt(sumCubes).toFixed(3);
+	return num;
 }
 
-console.log(countThis());
-
-// Task 5
-function changeText(e) {
-	if (typeof (e) == 'string') {
-		e = e.trim();
-		if (e.length > 50) {
-			e = e.slice(0, 50) + "...";
-		}
-	} else {
-		alert("Пожалуйста, введите строчное значение");
-	}
-	return e;
+// Функция рандомных чисел от min до max + 1
+function getRandomNum(min, max) {
+	return Math.floor(min + Math.random() * (max + 1 - min));
 }
 
-console.log(changeText("             Туман за окном, почти непроглядная белая пелена - идёт снег! Он покрыл землю, крыши и ветви деревьев, он пришёл на смену неприятному холоду и льду предшествующих дней, он зовёт гулять - погрузиться в этот влажный, полный белых пушинок воздух и идти, идти, идти - дыша свежестью             и превращаясь в сугроб, ловя момент - ибо сия прелесть недолговечна, и уже очень скоро будет либо мороз, либо тихая погода с серым и низким зимним небом, либо - месиво из снега, воды и грязи.                "));
+// Функция заполнения массива данными
+function fillArr() {
+	let countArr = countArrs();
+
+	for (let i = 0; i < countArr; i++) {
+		let randomElemArr = getRandomNum(1, 2);
+		arr[i] = [];
+
+		for (let j = 0; j < randomElemArr; j++)
+			arr[i][j] = getRandomNum(0, 50);
+	}
+}
+fillArr();
+console.log(arr);
+
+// Функция подсчета суммы всех элементов массива
+function sumArrs(array) {
+	let result = 0;
+
+	for (let i = 0; i < array.length; i++) {
+		for (let j = 0; j < array[i].length; j++) {
+			result += array[i][j];
+		}
+	}
+	return result;
+}
+
+console.log('Сумма элементов массива равна: ' + sumArrs(arr));
