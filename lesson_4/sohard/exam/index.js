1df1214c1cccd5c160470d741572985cafedf77d
Nikita Trofimov <nikita_givchik@mail.ru>
2019-11-08 14:39:57 +0300

    Доделал УЛЬТРАСЛОЖНОЕ задание в УРОКЕ № 4

--- lesson_4/sohard/exam/index.js
+++ lesson_4/sohard/exam/index.js
@@ -19,13 +19,62 @@
 // Числа должны находится в правильном порядке. [284, 220] - неправильно. Пары дружественных чисел не должны повторяться. [[220, 284], [284, 220]] - неправильно.
 
 
-function getFriendlyNumbers(start, end) {
-	if (isNaN(start) || isNaN(end) || start > end || start <= 0 || end <= 0 || Number.isInteger(start) || Number.isInteger(end)) {
-		return false;
-	} else {
+'use strict';
 
+// Функция, которая проверяет на дружественность 2 числа. Возвращает true если числа дружественны и false если нет
+function isFriendly(num1, num2) {
+	let sum1 = getDivisorsSum(num1),
+		sum2 = getDivisorsSum(num2);
+
+	if (sum1 == num2 && sum2 == num1) {
+		return true;
+	} else {
+		return false;
 	}
-	return []
+}
+
+// Общая функция для сразу двух действий: поиска делителей и поска суммы этих делителей. Возвращает итоговое значение суммы делителей числа
+function getDivisorsSum(num) {
+	return getSum(getDivisors(num));
+}
+
+// Функция поиска делителей числа. Возращает массив из делителей числа
+function getDivisors(num) {
+	let arr = [];
+	for (let i = 1; i < num; i++) {
+		if (num % i == 0) {
+			arr.push(i);
+		}
+	}
+	return arr;
+}
+
+// Функция суммы делителей числа. Возвращаем значение суммы числа
+function getSum(arr) {
+	let sum = 0;
+	for (let i = 0; i < arr.length; i++) {
+		sum += arr[i];
+	}
+	return sum;
+}
+
+// Функция, которая соединяет все другие функции и возвращает решение задачи
+function getFriendlyNumbers(start, end) {
+	let result = [];
+	if (typeof (start) === 'number' && typeof (end) === 'number' && start <= end && start > 0 && end > 0 && Number.isInteger(start) && Number.isInteger(end)) {
+		for (let i = start; i < end; i++) {
+			for (let j = i + 1; j <= end; j++) {
+				if (isFriendly(i, j) == true) {
+					result.push([i, j]);
+				} else if (result == []) {
+					return result;
+				}
+			}
+		}
+		return result;
+	} else {
+		return false;
+	}
 }
 
 module.exports = {
