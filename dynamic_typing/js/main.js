e48041a9ea041a845a257126fc2f5fe9b9ee5cab
Nikita Trofimov <nikita_givchik@mail.ru>
2019-11-10 19:54:44 +0300

    Обновил дополнительное задание по типизации

--- dynamic_typing/js/main.js
+++ dynamic_typing/js/main.js
@@ -2,12 +2,24 @@
 
 // Вывести в столбик все простые числа от 1 до 100. Рядом с каждым числом написать оба делителя данного числа. Например: “Делители этого числа: 1 и n”
 
+
+// Функция, которая проверяет является ли число простым
+function getSimple(num) {
+	if (num < 2) return false;
+	for (var i = 2; i < num; i++) {
+		if (num % i == 0)
+			return false;
+	}
+	return true;
+}
+
+// Главная функция для нахождения простых чисел в диапазоне от start до end
 function searhSimple(start, end) {
 	let result = [];
 
 	if (typeof (start) === 'number' && typeof (end) === 'number' && start <= end && start > 0 && end > 0 && Number.isInteger(start) && Number.isInteger(end)) {
 		for (let i = start; i <= end; i++) {
-			if (i % 1 == 0 && i % i == 0) {
+			if (getSimple(i) == true) {
 				result.push(i);
 			}
 		}
@@ -19,5 +31,4 @@ function searhSimple(start, end) {
 		alert("Вы неправильно заполнили данные");
 	}
 }
-
 searhSimple(1, 100);
