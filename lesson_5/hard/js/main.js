984fb38fd96f1861702eaaf9c88916355c1c9233
Nikita Trofimov <nikita_givchik@mail.ru>
2019-11-12 11:20:18 +0300

    Выполнил ДОПОЛНИТЕЛЬНОЕ задание в УРОКЕ № 5

--- lesson_5/hard/js/main.js
+++ lesson_5/hard/js/main.js
@@ -1,48 +1,63 @@
 'use strict';
 
-// Многомерные массивы ( [ [], [], ..] ) иногда называют матрицами. Напишите функцию, которая будет спрашивать у пользователя сколько массивов включить во внутрь arr, заполняет их несколькими произвольными числами (не больше 5) и выводит сумму элементов этого массива.
+// 1) Выведите на страницу текущую дату и время в формате '09:59:59 30.05.2018'
+function getTime() {
+	let date = new Date(),
+		seconds = setZeroBeforeDate(date.getSeconds()),
+		minutes = setZeroBeforeDate(date.getMinutes()),
+		hours = setZeroBeforeDate(date.getHours()),
+		days = setZeroBeforeDate(date.getDate()),
+		month = setZeroBeforeDate(date.getMonth()),
+		year = setZeroBeforeDate(date.getFullYear());
 
-let arr = [];
+	document.querySelector('.date-one').innerHTML = "Текущая дата и время: " + hours + ":" + minutes + ":" + seconds + " " + days + ":" + month + ":" + year;
+}
+getTime();
 
-// Функция, где спрашиваем количество массивов, которые хотим добавить в массив
-function countArrs() {
-	let num;
-	while (isNaN(num) || num == '' || num == null || num <= 0) {
-		num = +prompt("Сколько массивов включить в наш массив?");
+// 2) Напишите функцию, которая будет добавлять 0 перед днями и месяцами, которые состоят из одной цифры (из 1.6.2018 сделает 01.06.2018)
+function setZeroBeforeDate(item) {
+	if (item < 10) {
+		item = "0" + item;
+	} else {
+		item = item;
 	}
-	return num;
+	return item;
 }
 
-// Функция рандомных чисел от min до max + 1
-function getRandomNum(min, max) {
-	return Math.floor(min + Math.random() * (max + 1 - min));
+// 3) Напишите функцию, которая выводит на страницу текущий день недели на русском языке (реализацию выбираете сами)
+function showWeekDay() {
+	let days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
+		date = new Date();
+
+	document.querySelector('.date-two').innerHTML = "Текущий день недели: " + days[date.getDay()];
 }
+showWeekDay();
 
-// Функция заполнения массива данными
-function fillArr() {
-	let countArr = countArrs();
 
-	for (let i = 0; i < countArr; i++) {
-		let randomElemArr = getRandomNum(1, 2);
-		arr[i] = [];
+// 4) Напишите функцию, которая выводит на страницу разницу между двумя датами в количестве дней
 
-		for (let j = 0; j < randomElemArr; j++)
-			arr[i][j] = getRandomNum(0, 50);
+// Создаем переменные инпутов и кнопки
+let firstDate = document.querySelector('.date-input-one'),
+	secondDate = document.querySelector('.date-input-two'),
+	btnStart = document.querySelector('.button-result'),
+	inputResult = document.querySelector('.result');
+
+// Добавляем событие на нажатие кнопки
+btnStart.addEventListener('click', function () {
+	// Принимаем значения из инпутов в наши переменные с датами
+	let dateStart = firstDate.value,
+		dateEnd = secondDate.value,
+		result = 0;
+	//  Метод Date.parse(str) разбирает строку str в таком формате (YYYY-MM-DD – дата в формате год-месяц-день) и возвращает соответствующее ей количество миллисекунд
+	dateStart = Date.parse(dateStart);
+	dateEnd = Date.parse(dateEnd);
+
+	// Проверка на то, чтобы все инпуты с датами были заполнены
+	if (firstDate.value && secondDate.value) {
+		// Math.ceil - округляет полученный результат до целого числа, Math.abs делает число положительным (сделано для того, чтобы избежать случая, когда начальная дата больше конечной)
+		result = Math.ceil(Math.abs(dateEnd - dateStart)) / (1000 * 3600 * 24);
+		inputResult.value = result;
+	} else {
+		alert('Пожалуйста, введите даты еще раз');
 	}
-}
-fillArr();
-console.log(arr);
-
-// Функция подсчета суммы всех элементов массива
-function sumArrs(array) {
-	let result = 0;
-
-	for (let i = 0; i < array.length; i++) {
-		for (let j = 0; j < array[i].length; j++) {
-			result += array[i][j];
-		}
-	}
-	return result;
-}
-
-console.log('Сумма элементов массива равна: ' + sumArrs(arr));
+});
